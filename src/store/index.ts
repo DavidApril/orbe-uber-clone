c8ca6a9601dcf1bc604e8e0bab698109a9529f3c
David <davidagamez04@gmail.com>
2024-07-23 13:27:56 -0500

    delete: register client store deleted

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 export * from './auth/auth.store';
 export * from './permissions/permissions.store';
 export * from './location/location.store';
-export * from './client/register.store';
 export * from './driver/driver.store'
